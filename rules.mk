59ec656e08114c6fb984dc10ae06c5e0964038b3
Eduardo Bravo <mentzeralexander9@gmail.com>
2022-09-08 00:46:44 -0500

    Add README

---
+++ rules.mk
@@ -0,0 +1,37 @@
+BOOTLOADER = atmel-dfu
+NKRO_ENABLE = no
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+UNICODE_ENABLE = no		# Disable Unicode (special characters emojis and symbols)
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+EXTRAKEY_ENABLE = yes	# Allows media keys (music, brigthnesss and volume)
+SWAP_HANDS_ENABLE = no  # Allow swapping hands of keyboard
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+
+# Configuration for rgb
+
+
+# BOOTLOADER = atmel-dfu
+
+# EXTRAFLAGS += -flto
+
+# COMMAND_ENABLE = no # Commands for debug and configuration
+# UNICODE_ENABLE = no# Unicode
+# MOUSEKEY_ENABLE = no # Mouse keys(+4700)
+# SWAP_HANDS_ENABLE = no  # Allow swapping hands of keyboard
+
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = WS2812
+# BACKLIGHT_ENABLE = no
+# RGBLIGHT_ENABLE = no
+
+# NKRO_ENABLE = no
+
+# OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306
+
+# LTO_ENABLE = yes
